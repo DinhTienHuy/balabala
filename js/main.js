0aa7af6f255149836a4a8b04c772bd4e0b80bbd5
Huy Dinh <dinhtienhuy0909@gmail.com>
2024-02-15 09:45:19 +0700

    update code

--- js/main.js
+++ js/main.js
@@ -263,6 +263,144 @@ $(document).ready(function () {
     $(this).addClass("active");
     $(this).closest(".product__info-container").find(".sku-value").html(sku);
   });
+
+  // $('.slider-main').slick({
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   arrows: false,
+  //   fade: true,
+  //   asNavFor: '.slider-nav',
+  //   infinite: true,
+  // });
+  // $('.slider-nav').slick({
+  //   slidesToShow: 8,
+  //   slidesToScroll: 1,
+  //   asNavFor: '.slider-main',
+  //   dots: false,
+  //   focusOnSelect: true,
+  //   vertical: true,
+  //   verticalSwiping: true,
+  //   infinite: true,
+  //   draggable: true
+  // });
+  // $(window).on('resize orientationchange', function() {
+  //   $('.slider-nav').slick('unslick');
+  //   $('.slider-nav').slick({
+  //     slidesToShow: 8,
+  //     slidesToScroll: 1,
+  //     asNavFor: '.slider-main',
+  //     vertical: true,
+  //     focusOnSelect: true,
+  //     autoplay: false,
+  //     centerMode: true
+  //   });
+  // });
+
+  // var sync1 = $(".slider-main");
+  // var sync2 = $(".slider-nav");
+  // var slidesPerPage = 8;
+  // var syncedSecondary = true;
+
+  // sync1.owlCarousel({
+  //     items: 1,
+  //     slideSpeed: 2000,
+  //     nav: true,
+  //     autoplay: false, 
+  //     dots: false,
+  //     loop: true,
+  //     responsiveRefreshRate: 200,
+  //     navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+  // }).on('changed.owl.carousel', syncPosition);
+
+  // sync2.on('initialized.owl.carousel', function() {
+  //   sync2.find(".owl-item").eq(0).addClass("current");
+  // })
+  // .owlCarousel({
+  //   items: slidesPerPage,
+  //   dots: false,
+  //   nav: false,
+  //   smartSpeed: 200,
+  //   slideSpeed: 500,
+  //   slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
+  //   responsiveRefreshRate: 100
+  // }).on('changed.owl.carousel', syncPosition2);
+
+  // function syncPosition(el) {
+  //     //if you set loop to false, you have to restore this next line
+  //     //var current = el.item.index;
+
+  //     //if you disable loop you have to comment this block
+  //     var count = el.item.count - 1;
+  //     var current = Math.round(el.item.index - (el.item.count / 2) - .5);
+
+  //     if (current < 0) {
+  //         current = count;
+  //     }
+  //     if (current > count) {
+  //         current = 0;
+  //     }
+
+  //     //end block
+
+  //     sync2
+  //         .find(".owl-item")
+  //         .removeClass("current")
+  //         .eq(current)
+  //         .addClass("current");
+  //     var onscreen = sync2.find('.owl-item.active').length - 1;
+  //     var start = sync2.find('.owl-item.active').first().index();
+  //     var end = sync2.find('.owl-item.active').last().index();
+
+  //     if (current > end) {
+  //         sync2.data('owl.carousel').to(current, 100, true);
+  //     }
+  //     if (current < start) {
+  //         sync2.data('owl.carousel').to(current - onscreen, 100, true);
+  //     }
+  // }
+
+  // function syncPosition2(el) {
+  //     if (syncedSecondary) {
+  //         var number = el.item.index;
+  //         sync1.data('owl.carousel').to(number, 100, true);
+  //     }
+  // }
+
+  // sync2.on("click", ".owl-item", function(e) {
+  //     e.preventDefault();
+  //     var number = $(this).index();
+  //     sync1.data('owl.carousel').to(number, 300, true);
+  // });
+// 1st carousel, main
+// $('.slider-main').flickity();
+
+// $('.slider-nav').flickity({
+//   asNavFor: '.slider-main',
+//   contain: true,
+//   pageDots: false,
+//   verticalCells: true,
+//   groupCells: 8
+// });
+
+  // var galleryTop = new Swiper('.slider-main', {
+  //   spaceBetween: 10,
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+  // });
+  // var galleryThumbs = new Swiper('.slider-nav', {
+  //   spaceBetween: 10,
+  //   centeredSlides: true,
+  //   slidesPerView: '8',
+  //   touchRatio: 0.2,
+  //   slideToClickedSlide: true,
+  //   direction: "vertical",
+  // });
+
+
+  // galleryTop.controller.control = galleryThumbs;
+  // galleryThumbs.controller.control =galleryTop; 
   /*end*/
 
   //get height window
